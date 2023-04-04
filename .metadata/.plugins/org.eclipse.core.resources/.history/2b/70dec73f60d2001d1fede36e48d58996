42903cb65d3284f394ef142964ffe2b66e9f7caf
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2023-04-04 11:57:18 +0530

    14

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/2b/70dec73f60d2001d1fede36e48d58996
@@ -0,0 +1,34 @@
+package pg60;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+
+		int c, n, search, array[];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of element.");
+		n = sc.nextInt();
+		array = new int[n];
+
+		System.out.println("Enter " + n + " Integers");
+		for (c = 0; c < n; c++) {
+			array[c] = sc.nextInt();
+		}
+		System.out.println("Enter the value to find ");
+		search = sc.nextInt();
+
+		for (c = 0; c < n; c++) {
+			if (array[c] == search) {
+				System.out.println(search + " is present at location " + (c + 1) + ".");
+				break;
+			}
+			if (c != n) {
+				System.out.println(search + " is not availble in array.");
+			}
+		}
+		sc.close();
+	}
+
+}
--- Coding/src/pg60/LinearSearch.java
+++ Coding/src/pg60/LinearSearch.java
@@ -7,28 +7,29 @@ public class LinearSearch {
 	public static void main(String[] args) {
 
 		int c, n, search, array[];
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of element.");
-		n = sc.nextInt();
-		array = new int[n];
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the number of element.");
+			n = sc.nextInt();
+			array = new int[n];
 
-		System.out.println("Enter " + n + " Integers");
-		for (c = 0; c < n; c++) {
-			array[c] = sc.nextInt();
-		}
-		System.out.println("Enter the value to find ");
-		search = sc.nextInt();
+			System.out.println("Enter " + n + " Integers");
+			for (c = 0; c < n; c++) {
+				array[c] = sc.nextInt();
+			}
+			System.out.println("Enter the value to find ");
+			search = sc.nextInt();
 
-		for (c = 0; c < n; c++) {
-			if (array[c] == search) {
-				System.out.println(search + " is present at location " + (c + 1) + ".");
-				break;
+			for (c = 0; c < n; c++) {
+				if (array[c] == search) {
+					System.out.println(search + " is present at location " + (c + 1) + ".");
+					break;
+				}
+				if (c != n) {
+					System.out.println(search + " is not availble in array.");
+				}
 			}
-			if (c != n) {
-				System.out.println(search + " is not availble in array.");
-			}
+			sc.close();
 		}
-		sc.close();
 	}
 
 }
